7000a0df25b60afd4ea2d84c7577aedc9e44f7aa
HoutarouM <marian.horoshko@gmail.com>
2022-12-01 09:05:23 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NWD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calc_NWD(object sender, RoutedEventArgs e)
+        {
+            if(int.TryParse(num_a.Text, out int a))
+            {
+                if(int.TryParse(num_b.Text, out int b))
+                {
+
+                }
+            }
+        }
+    }
+}
